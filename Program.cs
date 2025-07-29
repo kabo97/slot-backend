199ac2c80d08198d5f04b0b9a7601da3aba1ce80
kabo97 <alielmatarawy8@gmail.com>
2025-07-29 12:29:57 +0300

    final commit

--- Program.cs
+++ Program.cs
@@ -9,32 +9,24 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-var configConn = builder.Configuration.GetConnectionString("DefaultConnection");
-var connectionString = configConn == "UseAzureEnv"
-    ? Environment.GetEnvironmentVariable("AZURE_DB_CONNECTION")
-    : configConn;
-
-builder.Services.AddDbContext<AppDbContext>(options =>options.UseNpgsql(connectionString));
+builder.Services.AddDbContext<AppDbContext>(options =>options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<ISlotService, SlotService>();
 builder.Services.AddControllers(); 
-builder.Services.AddCors(options => {
-    options.AddPolicy("AllowAll", builder =>
-        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", builder =>
+        builder.WithOrigins("https://slot-frontend.vercel.app")
+               .AllowAnyMethod()
+               .AllowAnyHeader());
 });
+
 var app = builder.Build();
-app.UseCors("AllowAll");
-
-// Configure the HTTP request pipeline.
-app.UseSwagger();
-app.UseSwaggerUI();
-
-
+app.UseCors("AllowFrontend");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 app.UseHttpsRedirection();
-
-
-
 app.MapControllers(); 
-
 app.Run();
-
-
--- obj/Debug/net8.0/BackendAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/BackendAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+374597933f29c6c535569b773849a0cefd267a14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae7381e18dc55dc04c9ef9d1e7195f455572d96f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
